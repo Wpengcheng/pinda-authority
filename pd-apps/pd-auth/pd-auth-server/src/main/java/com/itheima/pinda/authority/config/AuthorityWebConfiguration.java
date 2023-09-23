fbd213ef0020ef7bc830d060a8c2d575d4545d52
weipch <weipch5152@aliyun.com>
2023-09-23 20:58:27 +0800

    feat导入配置类和update启动类

---
+++ pd-apps/pd-auth/pd-auth-server/src/main/java/com/itheima/pinda/authority/config/AuthorityWebConfiguration.java
@@ -0,0 +1,13 @@
+package com.itheima.pinda.authority.config;
+
+import com.itheima.pinda.common.config.BaseConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author 方唐镜
+ * @Create 2023-09-23 20:54
+ * @Description
+ */
+@Configuration
+public class AuthorityWebConfiguration extends BaseConfig {
+}
